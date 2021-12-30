186a8813693a3d348b27e1f9871de960bc8a4899
dmtwood <dimitri.gevers@gmail.com>
2021-12-30 15:23:38 +0100

    back to main link from sign in form

--- app/src/main/java/be/dimitrigevers/android/geversdimitrinotifier/LoginActivity.kt
+++ app/src/main/java/be/dimitrigevers/android/geversdimitrinotifier/LoginActivity.kt
@@ -3,12 +3,30 @@ package be.dimitrigevers.android.geversdimitrinotifier
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        login_button_loginform.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.email_input_loginform)
+            val emailstring = email.getText().toString()
+            val password = findViewById<EditText>(R.id.password_input_loginform)
+            val password_string = password.getText().toString()
+
+            Log.d("MainActivity", "Email: " + emailstring)
+            Log.d("MainActivity", "Password: " + password_string)
+        }
+
+        back_to_main_link_loginform.setOnClickListener {
+            finish()
+        }
 
 
 
