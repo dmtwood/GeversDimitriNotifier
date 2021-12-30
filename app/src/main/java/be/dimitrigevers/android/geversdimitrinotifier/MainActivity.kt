7275e102d419adde8cdcc9124251b641c8f1008c
dmtwood <dimitri.gevers@gmail.com>
2021-12-30 13:01:42 +0100

    read register screen input - username, email and password - in MainActivity

--- app/src/main/java/be/dimitrigevers/android/geversdimitrinotifier/MainActivity.kt
+++ app/src/main/java/be/dimitrigevers/android/geversdimitrinotifier/MainActivity.kt
@@ -2,10 +2,31 @@ package be.dimitrigevers.android.geversdimitrinotifier
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val registerButton = findViewById<Button>(R.id.register_button_registerform)
+        registerButton.setOnClickListener {
+
+            // via synthetic view, not recommended anymore
+            val username = username_edittext_registerform.text.toString()
+            val email = findViewById<EditText>(R.id.email_input_registerform)
+            val emailstring = email.getText().toString()
+            val password = findViewById<EditText>(R.id.password_input_registerform)
+            val password_string = password.getText().toString()
+
+            Log.d("MainActivity", "Username: " + username)
+            Log.d("MainActivity", "Email: " + emailstring)
+            Log.d("MainActivity", "Password: " + password_string)
+
+        }
     }
 }
